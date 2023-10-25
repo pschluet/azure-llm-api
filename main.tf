94a3d79a14d8cef867e597e9aa6f4458258d559d
Paul Schlueter <ps589@ntrs.com>
2023-10-25 09:02:01 -0500

    Add Azure ML Workspace

--- main.tf
+++ main.tf
@@ -21,7 +21,45 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = "azure-llm-api"
   location = "eastus"
+}
+
+resource "azurerm_application_insights" "main" {
+  name                = "azure-llm-api"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "main" {
+  name                = "azure-llm-api-key-vault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "azurellmapi"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_machine_learning_workspace" "main" {
+  name                    = "azure-llm-api-workspace"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.main.id
+  key_vault_id            = azurerm_key_vault.main.id
+  storage_account_id      = azurerm_storage_account.main.id
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
